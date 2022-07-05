72d65582eb08f92b18bcac0a33f4a82dfda186be
Alex Zenin <alexander.zenin@reece.com.au>
2022-07-05 14:52:22 +1000

    build: add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,18 @@
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser",
-  extends: ["eslint:recommended", "plugin:react/recommended"],
-  plugins: [
-    '@typescript-eslint',
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
   ],
+  plugins: ['@typescript-eslint', 'react', 'prettier'],
   env: {
     browser: true,
     node: true,
     jest: true,
   },
   rules: {
-    "react/react-in-jsx-scope": "off"
-  }
+    'react/react-in-jsx-scope': 'off',
+  },
 }
